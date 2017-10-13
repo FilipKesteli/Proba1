f8167d153ca78d813c206589a6eb922b69c86b36
Filip Kesteli <filip.kesteli@gmail.com>
2017-10-13 17:08:58 +0200

    2.0-Drugi commit

--- DataTypes.R
+++ DataTypes.R
@@ -4,3 +4,21 @@
 #Arrays
 #Factors
 #Data Frames
+
+v1 <- TRUE
+print(class(v1))
+
+v2 <- 23.5
+print(class(v2)) 
+
+v3 <- 2L
+print(class(v3))
+
+v4 <- 2+5i
+print(class(v4))
+
+v5 <- "TRUE"
+print(class(v5))
+
+v <- charToRaw("Hello")
+print(class(v))
